ff6af9cf1b07ffa2ada88c736312ddd6a2d8275a
MuhiziLB <muhizi1997@gmail.com>
2024-01-22 15:11:04 +0300

    changed the logo

--- src/scripts/stickymenu.js
+++ src/scripts/stickymenu.js
@@ -16,9 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
     if(logo.length) {
       // === logo change
       if (ud_header.classList.contains('sticky-header')) {
-        document.querySelector(".header-logo").src = '/assets/logo/logo.svg';
+        document.querySelector(".header-logo").src = '/assets/kwizy/main-logo.jpg';
       } else {
-        document.querySelector('.header-logo').src = '/assets/logo/logo-white.svg';
+        document.querySelector('.header-logo').src = '/assets/kwizy/main-logo.jpg';
       }
     }
 
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
       if (logo.length) {
         // === logo change
         if (ud_header.classList.contains('sticky-header')) {
-          document.querySelector('.header-logo').src = '/assets/logo/logo-white.svg';
+          document.querySelector('.header-logo').src = '/assets/kwizy/main-logo.jpg';
         } 
       }
     }
